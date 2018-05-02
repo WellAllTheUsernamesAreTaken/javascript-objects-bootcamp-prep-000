f653508b4710c7f2e858cb4aed69c421366d016f
ncg201064@gmail.com Glascock <ncg201064@gmail.com>
2018-05-02 02:59:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,9 @@
 var playlist = { 'Bruno Mars': 'Gernade', 'Ed Sheren': 'Perfect'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, { [artistName]: songTitle });
+  //return Object.assign({}, playlist, { [artistName]: songTitle });
+  playlist[artistName] = songTitle;
+  return playlist;
 }
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
