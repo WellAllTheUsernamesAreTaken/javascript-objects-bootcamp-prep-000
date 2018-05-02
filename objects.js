cfc205da7169ffa049affb917b0da2e0edf752bb
ncg201064@gmail.com Glascock <ncg201064@gmail.com>
2018-05-02 03:43:00 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ var playlist = {
 };
 
 function updatePlaylist(playListToUpdate, artistName, songTitle) {
-  playListToUpdate[artistName] = Object.assign({}, playListToUpdate, { [artistName]: songTitle}); //playlist[artistName] = songTitle;
+  playListToUpdate[artistName] = Object.assign({}, playListToUpdate, { [artistName]: songTitle}); //playListToUpdate[artistName] = songTitle;
   return playListToUpdate;
 }
 function removeFromPlaylist(playListToUpdate, artistName) {
