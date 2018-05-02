fe5763d0210efce81fb074acda84b5543db4cc55
ncg201064@gmail.com Glascock <ncg201064@gmail.com>
2018-05-02 03:23:34 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,10 +2,9 @@ var playlist = { 'Bruno Mars': 'Gernade', 'Ed Sheren': 'Perfect'};
 
 function updatePlaylist(play, artistName, songTitle) {
   var update = {};
-  var u = {};
   update = Object.assign({}, play, { [artistName]: songTitle});
   return function UpdateObject(update) {
-    return Object.assign({}, playlist, u);
+    return Object.assign({}, playlist, update);
   };
   //return Object.assign({}, Update, playlist)
   //playlist[artistName] = songTitle;
